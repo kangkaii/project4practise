aa4da6f9fbf929cb3a217a781f1ce56de4e301d2
kangkai <644192628@qq.com>
2018-06-25 14:39:46 +0800

    ConcurrentModificationException！

--- src/main/java/kk/practise/List练习/ConcurrentModificationException异常信息.java
+++ src/main/java/kk/practise/List练习/ConcurrentModificationException异常信息.java
@@ -1,27 +1,55 @@
 package kk.practise.List练习;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by  kk on 2017/9/14.
+ * 此类说明 在ArrayList 遍历删除过程中
+ * 直接使用 list.remove(e),报错
+ * 解决办法01：使用迭代器遍历，用 iterator.remove()
+ * 解决办法02：使用CopyOnWriteArrayList，可以遍历中remove
+ * 解决办法03：使用java8 新方法 listOne.removeIf(i -> i.equals(a))
  */
 public class ConcurrentModificationException异常信息 {
     public static void main(String[] args) {
         List<Integer> listOne = new ArrayList<>();
         List<Integer> listTwo = new ArrayList<>();
+        List<Integer> listThree = new CopyOnWriteArrayList<>();
         for(int i=1;i<5;i++){
             listOne.add(i);
         }
         for(int i=2;i<6;i++){
             listTwo.add(i);
         }
-        listOne.forEach(o ->{
+        for(int i=2;i<6;i++){
+            listThree.add(i);
+        }
+
+        Integer a = 1;
+        listOne.removeIf(i -> i.equals(a));
+        /*listOne.forEach(o ->{
             if (listTwo.contains(o)) {
                 listOne.remove(o);
             }
+        });*/
+        Iterator<Integer> iterator =  listTwo.iterator();
+        while(iterator.hasNext()){
+            Integer integer = iterator.next();
+            if(integer==2)
+                iterator.remove();   //注意这个地方
+        }
+        listThree.forEach(o ->{
+            if (new Integer(3).equals(o)) {
+                listThree.remove(o);
+            }
         });
+
         System.out.println(listOne);
+        System.out.println(listTwo);
+        System.out.println(listThree);
 
     }
 
