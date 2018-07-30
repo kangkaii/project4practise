a64b807ba47aa412633958efee062ae00505ac0f
kangkai <644192628@qq.com>
2018-07-30 10:34:43 +0800

    序列化、数字转汉字工具

--- src/main/java/kk/practise/String类理解/Demo01.java
+++ src/main/java/kk/practise/String类理解/Demo01.java
@@ -16,7 +16,7 @@ public class Demo01 {
     }
 
     public static void main(String[] args) {
-        String str1 = new String("abc");
+        String str1 = new String("abc").intern();
         String str2 = "abc";
         String str3 = "123";
 
