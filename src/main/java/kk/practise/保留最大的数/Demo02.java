9d84d7583d1abc6235bf0d2e53e8593d76e14112
kangkai <644192628@qq.com>
2018-11-07 15:41:26 +0800

    好久没提交

--- src/main/java/kk/practise/保留最大的数/Demo02.java
+++ src/main/java/kk/practise/保留最大的数/Demo02.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
  */
 public class Demo02 {
     //只要发现一个数 小于其后面------>删除，直到 删除n个
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 
+        //写法错误！！！！！！！
         Scanner scanner = new Scanner(System.in);
         StringBuilder sb = new StringBuilder(scanner.next());//目标数字
         int n = scanner.nextInt();//去除数字的数量
@@ -40,6 +41,33 @@ public class Demo02 {
         }
         System.out.println(sb);
         System.out.println("结束length："+sb.length());
+    }
+
+    /**
+     * 已通过
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(scanner.next());//目标数字
+        int n = scanner.nextInt();//去除数字的数量
+        if (n >= sb.length()) {
+            System.out.println(0);
+            return;
+        }
+        int t = 0;//实际删除的数量
+        for (int i = 0; i < sb.length()-1 && t<n;) {
+            if (sb.charAt(i) < sb.charAt(i+1)) {
+                sb.deleteCharAt(i);
+                t++;
+                i = i-1 >= 0? i-1:0;
+            }else {
+                i++;
+            }
+        }
+        if (t<n) {
+            sb.delete(sb.length()-(n-t),sb.length());
+        }
+        System.out.println(sb);
     }
 
 
